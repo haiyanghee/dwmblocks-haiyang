2b68d72f9d0681d1828697c4c0061529f9c90cdc
Haiyang He <imhhy123@hotmail.com>
2020-09-06 11:13:56 -0600

    cant count.. now weather and news will actually update in 15min, instead of 15 hours lmao

--- blocks.h
+++ blocks.h
@@ -3,14 +3,21 @@
 // clang-format off
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    // resoures will display memory and cpu
 	{"", "dwm_resources.sh", 15, 0},
-	{"", "dwm_network.sh", 10, 1},
+    // since storage rarely updates, will update every one hour..
+	{"", "dwm_storage.sh", 3600, 0},
+    // display ethernet and wifi status
+	{"", "dwm_network.sh", 20, 1},
+    // display battery
 	{"", "dwm_battery.sh", 10, 2},
-	//{"", "dwm_alsa.sh | tr '\n' ' '", 0, 3},
+    // display volume
 	{"", "dwm_alsa.sh", 0, 3},
+    // update newsboat
+	{"", "dwm_news.sh", 900, 4},
     // update weather and news in every 15 min
-	{"", "dwm_news.sh", 54000, 4},
-	{"", "dwm_weather.sh | tr '\n' ' ' | sed 's/^[ \t]*//;s/[ \t]*$//'", 54000, 5},
+	{"", "dwm_weather.sh | tr '\n' ' ' | sed 's/^[ \t]*//;s/[ \t]*$//'", 900, 5},
+    // display date
 	{"", "date \"+%Y-%m-%d %a %I:%M %p\"", 20, 5}};
 
 // sets delimeter between status commands. NULL character ('\0') means no
